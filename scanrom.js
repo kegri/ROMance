14133a10e7d788d0daa5af7354b0356a87acfb02
Çağrı Himmetoğlu <54588856+kegri@users.noreply.github.com>
2021-09-28 21:03:53 +0300

    Changes and improvements on index and ui.

--- scanrom.js
+++ scanrom.js
@@ -1,26 +1,17 @@
 var fs = require("fs");
-
 var path = require("path");
-
 var remotePath = "/roms";
-
-var filePath = path.resolve('../../rom_cn');
-
 var fileArr = [];
-
 var files = fs.readdirSync(filePath);
-    
 var count = files.length;
 console.log(files)
-writeFile(files);
- 
-function writeFile(fileArr){
 
+writeFile(files);
+
+function writeFile(fileArr){
 	var data = [];
 	for(var i=0;i<fileArr.length;i++){
 		var f = fileArr[i].split('.');
-
-		data.push('["'+f[0]+'","roms/rom_cn/'+(fileArr[i])+'"]');
 	}
     var data = data.join(",\n");
     fs.writeFileSync("../../"+"filelist.txt",data+'\r\n',function(err){
