89e946c1fbfc8f1c82a18e3cff55efcabb0df91c
kegri <kegriined@gmail.com>
2021-09-28 19:01:23 +0300

    master

--- scanrom.js
+++ scanrom.js
@@ -6,14 +6,13 @@ var files = fs.readdirSync(filePath);
 var count = files.length;
 console.log(files)
 
-
 writeFile(files);
+
 function writeFile(fileArr){
 	var data = [];
 	for(var i=0;i<fileArr.length;i++){
 		var f = fileArr[i].split('.');
 	}
-    
     var data = data.join(",\n");
     fs.writeFileSync("../../"+"filelist.txt",data+'\r\n',function(err){
         if(err) throw err;
